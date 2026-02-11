199bbc3a9d4ee0b935e4d7867d801ed5667829ac
MartinGruter <Martin.Grter@iths.se>
2026-02-11 12:55:14 +0100

    Lagt till sql, model och repository för Customer.

--- src/main/java/se/iths/martin/javaverktygprojekt/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package se.iths.martin.javaverktygprojekt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.iths.martin.javaverktygprojekt.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
